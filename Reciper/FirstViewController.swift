ea8724f1bf0320abc761452d8e7a28aa5a7f2c77
Jan Erik van Woerden <janerikvw2006@gmail.com>
2018-01-10 11:31:27 +0100

    Initial Commit

---
+++ Reciper/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Reciper
+//
+//  Created by Jan Erik van Woerden on 10-01-18.
+//  Copyright © 2018 Jan Erik van Woerden. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Reciper/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Reciper
+//
+//  Created by Jan Erik van Woerden on 10-01-18.
+//  Copyright © 2018 Jan Erik van Woerden. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
