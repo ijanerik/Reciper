2e3f7e91ad029b10510673d4586fcb6b258b9856
Jan Erik van Woerden <janerikvw2006@gmail.com>
2018-01-11 14:58:45 +0100

    Update 2

---
+++ Reciper/ViewControllers/SettingsTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsTableViewController.swift
+//  Reciper
+//
+//  Created by Jan Erik van Woerden on 11-01-18.
+//  Copyright © 2018 Jan Erik van Woerden. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //
+    @IBAction func pressedLogout(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+
+}
